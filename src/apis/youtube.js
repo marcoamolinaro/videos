021a2edc925da80c0be20849fb90ae0fda78fb2f
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2019-10-19 16:36:55 -0300

    116. Putting it All Together

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 const KEY = 'AIzaSyCIBEdpoET1vHl0MlaBqZwleK-BlMv2Hi0';
 
 export default axios.create({
-    baseURL: https://www.googleapis.com/youtube/v3,
+    baseURL: "https://www.googleapis.com/youtube/v3",
     params: {
-        part: 'snippet',
-        maxResults: 5,
-        key: KEY
+      part: "snippet",
+      maxResults: 5,
+      key: KEY
     }
-});
+  });
