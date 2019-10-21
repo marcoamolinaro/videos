434d3f452d379221c2d90d722b7f66687bea07cc
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2019-10-21 13:15:52 -0300

    126. Displaying a Video Player

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,8 +5,13 @@ const VideoDetail = ({video}) => {
         return <div>Loading...</div>;
     }
 
+    const videoSrc=`https://www.youtube.com/embed/${video.id.videoId}`;
+
     return (
         <div>
+            <div className="ui emnbed">
+                <iframe src={videoSrc}/>
+            </div>
             <div className="ui segment">
                 <h4 className="header">{video.snippet.title}</h4>
                 <p>{video.snippet.description}</p>
